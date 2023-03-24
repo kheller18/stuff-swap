d204b266e48f075b9f3b4014e734f42efdcffe0d
kheller18 <keenanaheller@gmail.com>
2023-03-23 23:01:25 -0700

    base code added and functional. Logo displays on landing

---
+++ client/src/components/Engine.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Landing from './Landing';
+
+const Engine = () => {
+
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+};
+
+export default Engine;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from '../logos/stuff_swap.png';
+
+const Landing = () => {
+
+  return (
+    <div>
+      <img alt="StuffSwap logo" src={logo} />
+    </div>
+  );
+};
+
+export default Landing;
