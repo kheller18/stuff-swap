60d48123f6bfd7a6954c50dd4ca9311c7c1c1116
kheller18 <keenanaheller@gmail.com>
2023-03-27 20:31:31 -0700

    renamed image files for latest logo version. Added content in the footer component. Changed routes for different components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route  } from 'react-router-dom';
-import Engine from './components/Engine';
+// import Engine from './components/Engine';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import ShopCollections from './components/ShopCollections';
+import Main from './components/Main';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Header />
         <Switch>
-          <Route path='/' exact component={Engine} />
+          <Route path='/' exact component={Main} />
+          <Route path='/collection' exact component={ShopCollections} />
           {/* <Route path='/login' exact component={Engine} />
           <Route path='/signup' exact component={Engine} /> */}
         </Switch>
