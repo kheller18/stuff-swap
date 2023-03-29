706726e00ba7d6ac27ebc015471c99b29ea29952
kheller18 <keenanaheller@gmail.com>
2023-03-29 14:00:15 -0700

    added more updates to the cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from './Image';
 import Button from './Button';
 import logo from '../logos/stuff_swap.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from  '@fortawesome/free-solid-svg-icons';
+import { faXmark, faTrash } from  '@fortawesome/free-solid-svg-icons';
 import '../styles/Cart.css';
 
 const Cart = props => {
+  const [cartTotal, setCartTotal] = useState('0');
 
   return (
     <div className='cart-container'>
@@ -20,12 +21,8 @@ const Cart = props => {
       </div>
       <div className='cart-body'>
         <div className='cart-body-header'>
-          <div className='cart-body-header-left'>
-            2 items
-          </div>
-          <div className='cart-body-header-right'>
-            Clear all
-          </div>
+          <div className='cart-body-header-left'>3 items</div>
+          <div className='cart-body-header-right'>Clear all</div>
         </div>
         <div className='cart-body-items'>
           <div className='cart-body-item'>
@@ -34,8 +31,38 @@ const Cart = props => {
                 <Image className='cart-image' alt='' src={logo} />
               </div>
               <div className='cart-item-contents'>
-                <p>Bag N Tag #1</p>
-                <p>Audell Sabeti</p>
+                <p className='cart-item-name'>Bag N Tag #1</p>
+                <p className='cart-item-creator'>Audell Sabeti</p>
+                <p className='cart-item-creator-earnings'>Creator earnings: 5%</p>
+              </div>
+              <div className='cart-item-price'>0.20 ETH</div>
+              <div className='cart-item-remove'>
+                <FontAwesomeIcon className='close-cart' icon={faTrash} />
+              </div>
+            </div>
+          </div>
+          <div className='cart-body-item'>
+            <div className='cart-body-item-details'>
+              <div className='cart-body-image'>
+                <Image className='cart-image' alt='' src={logo} />
+              </div>
+              <div className='cart-item-contents'>
+                <p className='cart-item-name'>Bag N Tag #1</p>
+                <p className='cart-item-creator'>Audell Sabeti</p>
+                <p className='cart-item-creator-earnings'>Creator earnings: 5%</p>
+              </div>
+              <div className='cart-item-price'>0.20 ETH</div>
+            </div>
+          </div>
+          <div className='cart-body-item'>
+            <div className='cart-body-item-details'>
+              <div className='cart-body-image'>
+                <Image className='cart-image' alt='' src={logo} />
+              </div>
+              <div className='cart-item-contents'>
+                <p className='cart-item-name'>Bag N Tag #1</p>
+                <p className='cart-item-creator'>Audell Sabeti</p>
+                <p className='cart-item-creator-earnings'>Creator earnings: 5%</p>
               </div>
               <div className='cart-item-price'>0.20 ETH</div>
             </div>
@@ -43,10 +70,13 @@ const Cart = props => {
         </div>
       </div>
       <div className='cart-footer'>
-        <div>Total price</div>
-        <div>0.20 ETH</div>
+        <div className='cart-footer-top'>
+          <div className='cart-footer-price-label'>Total price</div>
+          <div className='cart-footer-price-eth'>{cartTotal} ETH</div>
+        </div>
+        <div className='cart-footer-price-usd'>{cartTotal} USD</div>
+        <Button className='purchase-btn'>Complete Purchase</Button>
       </div>
-      <Button className='purchase-btn'>Complete Purchase</Button>
     </div>
   );
 };
