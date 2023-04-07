37a186e8f2513334e5223437e10bc3b6a7122327
kheller18 <keenanaheller@gmail.com>
2023-04-06 20:35:40 -0700

    deleted unused server code

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const routes = require('./routes/api.js');
-// const passport = require('passport');
-// const User = require('./models/user');
-// const LocalStrategy = require('passport-local').Strategy;
-// const cron = require('node-cron');
-// const moment = require('moment');
 
 // defining the port for heroku or local
 const PORT = process.env.PORT || 3001;
@@ -23,9 +17,6 @@ const expressSession = require('express-session')({
 // serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // require('./seeders/herokuSeed')(db);
-  // require('./seeders/sportSeed');
-  console.log('product');
 } else {
   require('dotenv').config();
 }
@@ -33,14 +24,6 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(expressSession);
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-
-// setting up routes
-// app.use(routes);
 
 // define any API routes before this runs
 app.get("*", function(req, res) {
@@ -49,37 +32,6 @@ app.get("*", function(req, res) {
 
 // initializing server
 const server = require('http').createServer(app);
-
-// setting up socket
-// const io = require('socket.io')(server, {
-//   cors: {
-//     methods: ['GET', 'POST'],
-//     allowedHeaders: ['x-access-token', 'Origin', 'Content-Type', 'application/json'],
-//     credentials: true
-//   }
-// })
-
-// let loggedOnUsers = [];
-
-// when a user connects, this should give them the data they need for the betting component
-// io.on('connection', async (socket) => {
-//   if (socket.handshake.headers['x-current-user']) {
-//     loggedOnUsers[socket.handshake.headers['x-current-user']] = socket.id;
-//     // logsocket.handshake.headers['x-current-user']] = socket.id;
-//     // sendData = await fetchData(socket);
-//   }
-
-//   console.log('connection granted' , {
-//     socket: socket.id,
-//     userId: socket.handshake.headers['x-current-user']
-//   });
-
-//   socket.on('disconnect', () => {
-//     // console.log('user disconnected', socket.id)
-//     loggedOnUsers[socket.handshake.headers['x-current-user']] = null;
-//     // socket.removeAllListeners()
-//   });
-// });
 
 server.listen(PORT, () => {
   console.log(`🌎 ==> API Socket server now on port ${PORT}!`);
