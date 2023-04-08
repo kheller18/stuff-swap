7aaffd6d36d92886716d6464794761ed58c8e9f9
kheller18 <keenanaheller@gmail.com>
2023-04-07 18:01:51 -0700

    added comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Redirect, Route  } from 'react-router-dom';
-// import Engine from './components/Engine';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './App.css';
 import ShopCollections from './components/ShopCollections';
 import Main from './components/Main';
 import RegisterArtwork from './components/RegisterArtwork';
-import Wallet from './components/Wallet';
 import OwnedArtwork from './components/OwnedArtwork';
 import SellArtwork from './components/SellArtwork';
 import UpdateArtwork from './components/UpdateArtwork';
 import Art from './components/Art';
+import './App.css';
 
 function App() {
   const [walletAddress, setWalletAddress] = useState('');
   const [cart, setCart] = useState([]);
-  // console.log(walletAddress);
 
+  // defines routes for the site and their respective components and props
   return (
     <Router>
       <div className='App'>
-        {/* <Header userStatus={showUser} setUserStatus={setShowUser} cartStatus={showCart} setCartStatus={setShowCart} /> */}
         <Header content={cart} setContent={setCart} wallet={walletAddress} setWalletAddress={setWalletAddress} />
         <Routes>
           <Route path='/' exact element={<Main wallet={walletAddress} setWalletAddress={setWalletAddress} />} />
@@ -30,7 +27,6 @@ function App() {
           <Route path='/user/artwork/update' element={<UpdateArtwork wallet={walletAddress} setWalletAddress={setWalletAddress} />} />
           <Route path='/user/settings' element={<RegisterArtwork wallet={walletAddress} setWalletAddress={setWalletAddress} />} />
           <Route path='/artwork/item' element={<Art content={cart} setContent={setCart} wallet={walletAddress} setWalletAddress={setWalletAddress} />} />
-          {/* <Route path='/user/wallet' element={<Wallet wallet={walletAddress} setWalletAddress={setWalletAddress} />} /> */}
           <Route path='/user/artwork/owned' element={<OwnedArtwork wallet={walletAddress} setWalletAddress={setWalletAddress} />} />
           <Route path='/user/artwork/sell' element={<SellArtwork wallet={walletAddress} setWalletAddress={setWalletAddress} />} />
         </Routes>
